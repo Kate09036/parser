e2e42848a2af34a64a8a638bbb86f98e83b5c2b0
Kate09036 <katya.kucherr.kuterga@bk.ru>
2024-06-02 11:01:17 +0300

    new

--- nuton.py
+++ nuton.py
@@ -1,14 +1,14 @@
 def calculate_force(mass, acceleration):
-
     if mass < 0:
-        raise ValueError("Масса должна быть неотрицательной величиной..")
+        raise ValueError("Масса должна быть неотрицательной величиной.")
     if acceleration < 0:
         raise ValueError("Acceleration must be a non-negative value.")
     
     force = mass * acceleration
     return force
 
-mass = float(input("Введите массу тела(в kg): "))
+
+mass = float(input("Введите массу тела (в kg): "))
 acceleration = float(input("Введите ускорение (в m/s^2): "))
 
 try:
@@ -16,3 +16,4 @@ try:
     print(f"Сила, необходимая для ускорения объекта, равна {force:.2f} Н.")
 except ValueError as e:
     print(e)
+
